e26f61e830ba364592538e27cf3a95335f6b5ef8
marzia0011 <48626020+marzia0011@users.noreply.github.com>
2023-01-11 13:02:50 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<head>
+   <meta charset="UTF-8">
+   <title>CHAMPION_FEMALE</title>
+   <link rel="stylesheet" href="./styles.css">
+</head>
+<body> 
+<img src="images/logo1.jpg" 
+    weight = 750;
+    height =350;
+    alt="color instruments"> 
+    
+
+<?php 
+echo "<h1 style ='color:red;'>Female Footballer</h1>";
+?>  
+<?php
+echo "<h3 style = 'color:blue;'>
+Female Footballers is an non profit organization developed to help young girls focus on the mental aspects of sports.
+You would play football to entertain the fans and gain success for your team. You'd work hard to keep yourself fit and healthy and constantly practice to improve your skills. 
+You'd need to have exceptional footballing talent and the discipline to work hard to improve your performance level.
+</h3>";?>
+
+
+<?php 
+echo "<h2 style ='color:green;'>Who Wins 2022?</h2>";
+?> 
+<?php
+echo "<h3 style = 'color:red;'>
+Women's Ballon d'Or: Alexia Putellas wins award for best female footballer in 2022 - BBC Sport.
+Barcelona captain Alexia Putellas has retained the Women's Ballon d'Or, awarded to the best best female footballer of 2022.The midfielder, 28, who won the award last year, suffered an anterior cruciate ligament injury in the summer and missed Spain's Euro 2022 campaign.
+</h3>";?>
+
+
+
+<?php 
+echo "<h2 style ='color:green;'>Who Wins 2021?</h2>";
+?> 
+<?php
+echo "<h3 style = 'color:red;'>
+Women's Ballon d'Or: Alexia Putellas wins award for best female footballer in 2021- BBC Sport.
+</h3>";?>
+
+
+
+
+<?php 
+echo "<h2 style ='color:green;'>Alexia Putellas?</h2>";
+?> 
+<?php
+echo "<h3 style = 'color:red;'>
+Alexia Putellas Segura (Catalan: Alèxia Putellas i Segura,[3][4][5][6] pronounced [alɛksˈiə puteʎəs];[7] born 4 February 1994) is a Spanish professional footballer who plays as a midfielder for Barcelona, which she captains, and the Spain national team.[8] She previously played for Espanyol and Levante, and has represented Catalonia. Having won all major club and individual awards available to a European player by 2022,[9][10] she is widely regarded as the best contemporary female footballer in the world, and one of the greatest of all time.[11][12][13][14][15]
+
+Putellas has played for Barcelona since 2012, after spending most of her youth career in the ranks of Espanyol. With Barcelona, she has won six league titles, seven Copas de la Reina, and one UEFA Women's Champions League title. In Barcelona's 2020–21 season, she played an essential role as her team won the Champions League as well as the resulting continental treble, both for the first time in their history. Putellas then went on to win the UEFA Women's Player of the Year Award, the Ballon d'Or Féminin, and The Best FIFA Women's Player in 2021, becoming the first player to win all three in the same year. In 2022, despite missing the UEFA Women's Euro 2022 due to injury, she won the UEFA Women's Player of the Year Award and the Ballon d'Or Féminin, each for a second consecutive year, making her the first woman to do so.[9]
+
+On the international stage, Putellas had success with Spain's youth national teams, winning two UEFA Women's U-17 Euros (in 2010 and 2011) as well as finishing second place in the 2012 UEFA Women's U-19 Euro. She made her debut for Spain's senior national team in 2013, and has since featured in three major international competitions with the team: Spain's FIFA Women's World Cup debut in 2015, the 2017 UEFA Women's Euro, and the 2019 World Cup.
+
+As of 2022, Putellas has the second-most all-time appearances for Barcelona behind former left back Melanie Serrano, and is currently their second-highest all-time scorer behind former striker Jennifer Hermoso.[16][17][18] She also holds the record of most appearances for Spain with 100 caps, having surpassed Marta Torrejón's previous record of 90 caps in 2021.
+</h3>";?>
+
+
+<?php 
+echo "<h2 style ='color:green;'> characteristics of a footballer?
+</h2>";
+?> 
+<?php
+echo "<h3 style = 'color:red;'>
+
+1)Speed.
+2)strength.
+3)acceleration.
+4)agility. 
+5)sheer power are all very important in the modern game, but they will develop naturally during the early years of a player's development.
+6)Much more important at a young age is the development of technical skills.
+</h3>";?>
+
+
+<?php 
+echo "<h2 style ='color:green;'>A player profile can include:</h2>";
+?> 
+<?php
+echo "<h3 style = 'color:red;'>
+1)Personal details, including emergency contact information.
+2)Physical attributes and parameters.
+3)General health, illnesses, and allergies.
+4)Injury screening (past and present)
+5)Personality traits and coachability.
+</h3>";?>
+
+<?php 
+echo "<h2 style ='color:green;'>Is football good for girl?</h2>";
+?> 
+<?php
+echo "<h3 style = 'color:red;'>
+Beyond being a ball sport, it's an activity that works on endurance and cardio through sequences of running. 
+Football movements and techniques are physically demanding and help build tone and coordination.
+And as a good team sport, football has many benefits for your mental development!
+</h3>";?>
+
+<?php 
+echo "<h2 style ='color:green;'> write a soccer player CV?</h2>";
+?> 
+<?php
+echo "<h3 style = 'color:red;'>
+A senior soccer player should demonstrate their leadership skills throughout their experience section. 
+Also, it's vital to state clearly the name of the team, the number of years you played, the positions you played, and the number of goals & assists at the beginning of each experience section.
+</h3>";?>
+
+<script src="./main.js"></script>  
+
+
+
